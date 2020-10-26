f1a631e2c6599f0c4702be634f8819dd1b39a5a6
alan <ast9501@gmail.com>
2020-10-26 21:47:25 +0800

    Add Longterm mission register function

--- MissionCreate/views.py
+++ MissionCreate/views.py
@@ -15,15 +15,6 @@ def register_short_mission(request):
         if request.method == 'POST':
             json_data = json.loads(request.body)
             name = json_data['name']
-            # check if mission have been created
-            #nameList = ShortermMissionList.objects.all()
-            #for i in range (len(nameList)-1):
-            #    if nameList[i] == name:
-            #        error_message = "The mission have been created !"
-            #        text = error_message
-            #        return JsonResponse({'status': result, 'message': error_message})
-            #        break
-            # Null value
             emergency_level = int(json_data['emergency_level'])
             important_level = int(json_data['important_level'])
             try:
@@ -63,9 +54,61 @@ def register_short_mission(request):
             message = "Created failed, the same shorterm mission is available!"
         except:
             message = "Created failed, check out the following required field: name, expected_finish_time, emergency_level, important_level"
-        #try:
-        #    if (ShortermMissionList.objects.get(name = name) != None):
-        #        message = "The mission have been created! "
-        #except:
-        #    message = "Create failed, unknown error"
     return JsonResponse({'status': status, 'message': message, 'exist shorterm mission': shorterm_mission_count})
+
+# Longterm mission register, the same as shorterm mission register
+def register_long_mission(request):
+    status = 'failed'
+    message = 'Longterm mission created !'
+    Longterm_mission_count = 0
+    try:
+        if request.method == 'POST':
+            json_data = json.loads(request.body)
+            name = json_data['name']
+            emergency_level = int(json_data['emergency_level'])
+            important_level = int(json_data['important_level'])
+            try:
+                related_link_1 = json_data['related_link_1']
+            except:
+                related_link_1 = "127.0.0.1"
+
+            try:
+                related_link_2 = json_data['related_link_2']
+            except:
+                related_link_2 = "127.0.0.1"
+
+            # In longterm mission, it can have up to three releated link
+            try:
+                related_link_3 = json_data['related_link_3']
+            except:
+                related_link_3 = "127.0.0.1"
+
+            try:
+                assign_time = json_data['assign_time']
+            except:
+                assign_time = datetime.date.today()
+            try:
+                launch_time = json_date['launch_time']
+            except:
+                launch_time = datetime.date.today()
+
+            LongtermMissionList.objects.create(name = name, 
+                    assign_time = assign_time,
+                    launch_time = launch_time,
+                    expected_finish_time = json_data['expected_finish_time'],
+                    emergency_level = json_data['emergency_level'],
+                    important_level = json_data['important_level'],
+                    detail = json_data['discription'],
+                    related_link_1 = related_link_1,
+                    related_link_2 = related_link_2,
+                    related_link_3 = related_link_3
+                )
+        status = "Success"
+        longerm_mission_count = len(LongtermMissionList.objects.all())
+    except:
+        try:
+            LongtermMissionList.objects.get(name=name)
+            message = "Created failed, the same longterm mission is available!"
+        except:
+            message = "Created failed, check out the following required field: name, expected_finish_time, emergency_level, important_level"
+    return JsonResponse({'status': status, 'message': message, 'exist longterm mission': longterm_mission_count})
